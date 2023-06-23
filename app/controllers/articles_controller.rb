a9ea1494ca63514e1a18a2061855c423457ce8ab
Rutani Thakkar <rutanithakkar105@gmail.com>
2023-06-23 19:53:53 +0530

    Feat: Added a active stoarge

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,6 +43,6 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :image)
   end
 end  
